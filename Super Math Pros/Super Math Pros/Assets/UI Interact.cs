3f16534e80c7ad30f92fccf793c98bfbc36711a7
calcav28 <cavanagh.calvin@gmail.com>
2022-10-04 10:03:55 -0700

    modified UI Interact

--- Super Math Pros/Super Math Pros/Assets/UI Interact.cs
+++ Super Math Pros/Super Math Pros/Assets/UI Interact.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class UIInteract : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Button yourButton
     void Start()
     {
-        
+        Button btn = yourButton.GetComponent<Button>();
+        btn.conClick.AddListened(TaskOnClick);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //add move camera to next screen here 
     }
 }
